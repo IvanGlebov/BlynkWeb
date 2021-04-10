098023e1335d09c0a49211cd67602b66ec4f7262
IvanGlebov <glebov.vanya8@gmail.com>
2021-04-10 19:09:34 +0300

    Placeholder page container

---
+++ web-client/src/components/MainPage/MainPageContainer.jsx
@@ -0,0 +1,17 @@
+import {connect} from "react-redux";
+import MainPage from "./MainPage";
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+const MainPageContainer = connect(mapStateToProps, mapDispatchToProps)(MainPage)
+export default MainPageContainer
