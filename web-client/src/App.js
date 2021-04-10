f35c547a78c173855a3f2d9dc298e14c04ec3b9d
IvanGlebov <glebov.vanya8@gmail.com>
2021-04-10 19:06:57 +0300

    Added placeholder for / page

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import {Route} from "react-router-dom"
-import HeaderContainer from "./components/Header/HeaderContainer";
+// import HeaderContainer from "./components/Header/HeaderContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import DashboardContainer from "./components/Dashboard/DashboardContainer";
 import SettingsContainer from "./components/Settings/SettingsContainer";
+import MainPageContainer from "./components/MainPage/MainPageContainer";
 
 function App() {
     return (
         <div className='appWrapper'>
             <NavbarContainer/>
             <div className='appWrapperContent'>
+                <Route path='/' render={() => <MainPageContainer />} />
                 <Route path='/dashboard' render={() => <DashboardContainer />}/>
                 <Route path='/settings' render={() => <SettingsContainer />}/>
             </div>
---
+++ web-client/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import s from './MainPage.module.css'
+
+
+
+class MainPage extends React.Component{
+    render(){
+        return (
+            <div className={s.contentWrapper}>
+                Placeholder
+            </div>
+        )
+    }
+
+}
+
+export default MainPage
