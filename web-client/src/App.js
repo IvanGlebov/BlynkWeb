e65c501604598703cd20c5f95061df7c6ca7ba18
IvanGlebov <glebov.vanya8@gmail.com>
2021-04-29 17:10:34 +0300

    Default route fix

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,19 +1,18 @@
-// import logo from './logo.svg';
 import './App.css';
-import {Route} from "react-router-dom"
-// import HeaderContainer from "./components/Header/HeaderContainer";
+import {Route, Redirect} from "react-router-dom"
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import DashboardContainer from "./components/Dashboard/DashboardContainer";
 import SettingsContainer from "./components/Settings/SettingsContainer";
-import MainPageContainer from "./components/MainPage/MainPageContainer";
 
 function App() {
     return (
         <div className='appWrapper'>
             <NavbarContainer/>
             <div className='appWrapperContent'>
-                <Route path='/' render={() => <MainPageContainer />} />
+
+                <Route path='/' render={()=> <Redirect to='/dashboard' />}/>
                 <Route path='/dashboard' render={() => <DashboardContainer />}/>
+
                 <Route path='/settings' render={() => <SettingsContainer />}/>
             </div>
         </div>
